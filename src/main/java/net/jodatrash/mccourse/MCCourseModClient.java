b0a6c893fc4c586036b6488637e9ba1f9fe3ccc3
Jodatrash <JonathanPachecoCruz@hotmail.com>
2023-11-09 21:43:21 -0600

    (added) New class MCCourseModClient

---
+++ src/main/java/net/jodatrash/mccourse/MCCourseModClient.java
@@ -0,0 +1,12 @@
+package net.jodatrash.mccourse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCourseModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        throw new UnsupportedOperationException("Unimplemented method 'onInitializeClient'");
+    }
+
+}
