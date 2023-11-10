0a77a6680b856d71cb9df104511465134c30fd99
Jodatrash <JonathanPachecoCruz@hotmail.com>
2023-11-10 12:03:29 -0600

    (update) add ModItems to the method onInitialize

--- src/main/java/net/jodatrash/mccourse/MCCourseMod.java
+++ src/main/java/net/jodatrash/mccourse/MCCourseMod.java
@@ -1,6 +1,7 @@
 package net.jodatrash.mccourse;
 
 import net.fabricmc.api.ModInitializer;
+import net.jodatrash.mccourse.item.ModItems;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,7 @@ public class MCCourseMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItem();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
