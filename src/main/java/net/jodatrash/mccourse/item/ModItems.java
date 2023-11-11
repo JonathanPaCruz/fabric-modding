fc8baa6af593e057f9649b3d075e924dbeadac5a
Jodatrash <JonathanPachecoCruz@hotmail.com>
2023-11-10 23:42:23 -0600

    (update) Added new items

--- src/main/java/net/jodatrash/mccourse/item/ModItems.java
+++ src/main/java/net/jodatrash/mccourse/item/ModItems.java
@@ -13,8 +13,17 @@ import net.minecraft.util.Identifier;
 public class ModItems {
 
     public static final Item IRON_NEEDLE = registerItem("iron_needle",
-            new Item(new FabricItemSettings().maxCount(1)));
+            new Item(new FabricItemSettings().maxCount(16)));
     public static final Item BONE_NEEDLE = registerItem("bone_needle",
+            new Item(new FabricItemSettings().maxCount(16)));
+    public static final Item PHIAL = registerItem("phial",
+            new Item(new FabricItemSettings()));
+    public static final Item PHIAL_NEEDLE = registerItem("phial_needle",
+            new Item(new FabricItemSettings().maxCount(1)));
+    public static final Item PHIAL_FULL = registerItem("phial_full",
+            new Item(new FabricItemSettings().maxCount(1)));
+    // Item creativo
+    public static final Item INGREDIENT_PHIAL = registerItem("ingredient_phial",
             new Item(new FabricItemSettings().maxCount(1)));
 
     private static Item registerItem(String name, Item item) {
@@ -24,6 +33,11 @@ public class ModItems {
     private static void itemGroupIngredients(FabricItemGroupEntries entries) {
         entries.add(IRON_NEEDLE);
         entries.add(BONE_NEEDLE);
+        entries.add(PHIAL);
+        entries.add(PHIAL_NEEDLE);
+        entries.add(PHIAL_FULL);
+        // item creativo
+        entries.add(INGREDIENT_PHIAL);
     }
 
     public static void registerModItem() {
