037c1dd3ff6f5cd7dfa20f52de47af752793a541
Jodatrash <JonathanPachecoCruz@hotmail.com>
2023-11-14 13:42:28 -0600

    (update) Add new blocks in ModBlocks

--- src/main/java/net/jodatrash/mccourse/block/ModBlocks.java
+++ src/main/java/net/jodatrash/mccourse/block/ModBlocks.java
@@ -12,23 +12,37 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
-    // Create a block
-    public static final Block GRAVEL_IRON_ORE = registerBlock("gravel_iron_ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.GRAVEL)));
+        // Create a block
+        public static final Block GRAVEL_IRON_ORE = registerBlock("gravel_iron_ore",
+                        new Block(FabricBlockSettings.copyOf(Blocks.GRAVEL)));
+        public static final Block GRAVEL_COAL_ORE = registerBlock("gravel_coal_ore",
+                        new Block(FabricBlockSettings.copyOf(Blocks.GRAVEL)));
+        public static final Block SAND_IRON_ORE = registerBlock("sand_iron_ore",
+                        new Block(FabricBlockSettings.copyOf(Blocks.SAND)));
+        public static final Block SAND_COAL_ORE = registerBlock("sand_coal_ore",
+                        new Block(FabricBlockSettings.copyOf(Blocks.SAND)));
+        public static final Block REDSAND_IRON_ORE = registerBlock("redsand_iron_ore",
+                        new Block(FabricBlockSettings.copyOf(Blocks.SAND)));
+        public static final Block REDSAND_COAL_ORE = registerBlock("redsand_coal_ore",
+                        new Block(FabricBlockSettings.copyOf(Blocks.SAND)));
+        public static final Block SOLID_EXPERIENCE_BLOCK = registerBlock("solid_experience_block",
+                        new Block(FabricBlockSettings.copyOf(Blocks.AMETHYST_BLOCK)));
+        public static final Block SOLID_EXPERIENCE_CROP = registerBlock("solid_experience_crop",
+                        new Block(FabricBlockSettings.copyOf(Blocks.AMETHYST_BLOCK)));
 
-    // Create a register to the block
-    private static Block registerBlock(String name, Block block) {
-        registerBlockItem(name, block);
-        return Registry.register(Registries.BLOCK, new Identifier(MCCourseMod.MOD_ID, name), block);
-    }
+        // Create a register to the block
+        private static Block registerBlock(String name, Block block) {
+                registerBlockItem(name, block);
+                return Registry.register(Registries.BLOCK, new Identifier(MCCourseMod.MOD_ID, name), block);
+        }
 
-    // Create a new item to the block and register this item
-    private static Item registerBlockItem(String name, Block block) {
-        return Registry.register(Registries.ITEM, new Identifier(MCCourseMod.MOD_ID, name),
-                new BlockItem(block, new FabricItemSettings()));
-    }
+        // Create a new item to the block and register this item
+        private static Item registerBlockItem(String name, Block block) {
+                return Registry.register(Registries.ITEM, new Identifier(MCCourseMod.MOD_ID, name),
+                                new BlockItem(block, new FabricItemSettings()));
+        }
 
-    public static void registerModBlocks() {
-        MCCourseMod.LOGGER.info("Registering ModBlocks for " + MCCourseMod.MOD_ID);
-    }
+        public static void registerModBlocks() {
+                MCCourseMod.LOGGER.info("Registering ModBlocks for " + MCCourseMod.MOD_ID);
+        }
 }
